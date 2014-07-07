f4c180afd6a25285a20ae706a0ae51a703d1be57
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-06 23:14:03 -0400

    Implement AcuatorRele header

--- daemon/profiles/ActuatorRele/ActuatorRele.h
+++ daemon/profiles/ActuatorRele/ActuatorRele.h
@@ -2,15 +2,13 @@
 #ifndef ACTUATORRELE_H
 #define ACTUATORRELE_H
 
-extern "C" {
-	#include "serial.h"
-}
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <iostream>
+
+#include "ModCom.h"
 
 #define ENABLE_INTR		48
 #define DISABLE_INTR	56
@@ -18,13 +16,11 @@ extern "C" {
 #define	ENABLE_ALL		44
 #define	DISABLE_ALL		52
 
-class ActuatorRele {
-	int fd;
+class ActuatorRele : public ModDev{
+	
+	ModCom mc;
 
-	int n_suckers;
-
-	uint8_t buffer_in[255];
-	char buffer_out[255];
+	int n_rele;
 
 public:
 
@@ -33,7 +29,6 @@ public:
 
 	void enableRele(int index);
 	void disableRele(int index);
-
 	void enableAll();
 	void disableAll();
 
