8b57826963ff95f18a9541c61d671b33bb18acc6
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-11 01:14:53 -0400

    Prueba de ledcontrol
    Se implmenta prueba exitosa de led control.

--- daemon/src/test.cpp
+++ daemon/src/test.cpp
@@ -12,18 +12,25 @@
 #include "ModDev.h"
 #include "ModCom.h"
 #include "ActuatorRele.h"
+#include "LedControl.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-	ModCom mc1;
+	//ModCom mc1;
 	ModCom mc;
 	cout << mc.getModDescription() << '\n';
 
 	if (mc.getModDescription() == "ACT_RELE") {
 		ActuatorRele actr;
 		actr.asociateModCom(mc);
-		actr.enableRele(1);
+		actr.enableRele();
+	}
+	if (mc.getModDescription() == "LED_CTRL") {
+		LedControl lctrl;
+		lctrl.asociateModCom(mc);
+		lctrl.turnOn();
+		lctrl.setBrightness(50);
 	}
 
 	//printf("%d\n", (int)(mc.isAssigned()));
