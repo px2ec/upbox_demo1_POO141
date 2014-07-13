eef55c7666355abfa905d70d4d4167e8f3fde463
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-13 02:47:33 -0400

    Se prueba nuevas implementaciones
    Se hace la prueba con la clase Dommo y todos los perfiles de dispositivos.

--- daemon/src/test.cpp
+++ daemon/src/test.cpp
@@ -13,24 +13,42 @@
 #include "ModCom.h"
 #include "ActuatorRele.h"
 #include "LedControl.h"
+#include "TempSensor.h"
+
+#include "Dommo.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-	//ModCom mc1;
-	ModCom mc;
-	cout << mc.getModDescription() << '\n';
 
-	if (mc.getModDescription() == "ACT_RELE") {
-		ActuatorRele actr;
-		actr.asociateModCom(mc);
-		actr.enableRele();
-	}else if (mc.getModDescription() == "LED_CTRL") {
-		LedControl lctrl;
-		lctrl.asociateModCom(mc);
-		lctrl.turnOn();
-		lctrl.setBrightness(20);
+	Dommo dm1;
+	
+	while (1){
+		ModCom *mc = new ModCom;
+		if (!(mc->isAssigned())){
+			delete mc;
+			break;	
+		} 
+		cout << mc->getModDescription() << '\n';
+
+		if (mc->getModDescription() == "ACT_RELE") {
+			ActuatorRele *actr = new ActuatorRele;
+			actr->asociateModCom(*mc);
+			dm1.addModdev(*actr);
+		}else if (mc->getModDescription() == "LED_CTRL") {
+			LedControl *lctrl = new LedControl;
+			lctrl->asociateModCom(*mc);
+			dm1.addModdev(*lctrl);
+		}else if (mc->getModDescription() == "TMP_SENS") {
+			TempSensor *tmpsens = new TempSensor;
+			tmpsens->asociateModCom(*mc);
+			dm1.addModdev(*tmpsens);
+		}
 	}
 
-	//printf("%d\n", (int)(mc.isAssigned()));
+	vector<ModDev*> mdlist = dm1.getAllModDevs();
+
+	for (int i = 0; i < dm1.getModDevsCount(); ++i) {
+		cout << mdlist[i]->getDescription() << '\n';
+	}
 }
