a2c393d99037b376b40710ffa96a051fc639fbef
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-13 02:58:25 -0400

    Implementando...
    Se ponen las lines que funcionan, cosa super util para el demonio.

---
+++ daemon/src/upboxd.cpp
@@ -0,0 +1,36 @@
+
+#include "upboxd.h"
+
+int main(int argc, char const *argv[]) {
+
+	Dommo dm1;
+	
+	while (1){
+		ModCom *mc = new ModCom;
+		if (!(mc->isAssigned())){
+			delete mc;
+			break;	
+		} 
+		cout << mc->getModDescription() << '\n';
+
+		if (mc->getModDescription() == "ACT_RELE") {
+			ActuatorRele *actr = new ActuatorRele;
+			actr->asociateModCom(*mc);
+			dm1.addModdev(*actr);
+		}else if (mc->getModDescription() == "LED_CTRL") {
+			LedControl *lctrl = new LedControl;
+			lctrl->asociateModCom(*mc);
+			dm1.addModdev(*lctrl);
+		}else if (mc->getModDescription() == "TMP_SENS") {
+			TempSensor *tmpsens = new TempSensor;
+			tmpsens->asociateModCom(*mc);
+			dm1.addModdev(*tmpsens);
+		}
+	}
+
+	vector<ModDev*> mdlist = dm1.getAllModDevs();
+
+	for (int i = 0; i < dm1.getModDevsCount(); ++i) {
+		cout << mdlist[i]->getDescription() << '\n';
+	}
+}
