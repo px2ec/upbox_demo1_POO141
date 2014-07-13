34cf6185aca03a1d838818bce85d2c41596c49e5
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-13 12:34:16 -0400

    Se implementa socket y json
    Se escriben las funciones relacionadas con la manipulación de sockets y el uso de json.

--- daemon/include/upboxd.h
+++ daemon/include/upboxd.h
@@ -9,6 +9,12 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <cstring>      // Needed for memset
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
+
+#include <json/json.h>
+
 #include "ModDev.h"
 #include "ModCom.h"
 #include "ActuatorRele.h"
