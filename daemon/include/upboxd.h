84a473f2ef0d52bae5b2201126871f2bc968a5fb
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-13 02:57:51 -0400

    Moving files & implement dependencies

---
+++ daemon/include/upboxd.h
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <time.h>
+#include <unistd.h>
+
+#include "ModDev.h"
+#include "ModCom.h"
+#include "ActuatorRele.h"
+#include "LedControl.h"
+#include "TempSensor.h"
+
+#include "Dommo.h"
+
+using namespace std;
