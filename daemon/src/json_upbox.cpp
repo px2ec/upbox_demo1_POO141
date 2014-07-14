35a095290c39cd8815f2f2abef635555805dd5c7
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-14 01:08:24 -0400

    Se eliminan funciones innecesarias

--- daemon/src/json_upbox.cpp
+++ daemon/src/json_upbox.cpp
@@ -1,10 +1,11 @@
 #include "json_upbox.h"
 
-/*printing the value corresponding to boolean, double, integer and strings*/
+/*
+//printing the value corresponding to boolean, double, integer and strings//
 void print_json_value(json_object *jobj) {
 	enum json_type type;
 	printf("type: %d ",type);
-	type = json_object_get_type(jobj); /*Getting the type of the json object*/
+	type = json_object_get_type(jobj); //Getting the type of the json object//
 	switch (type) {
 		case json_type_boolean: printf("json_type_boolean\n");
 			printf("value: %s\n", json_object_get_boolean(jobj)? "true": "false");
@@ -22,21 +23,21 @@ void print_json_value(json_object *jobj) {
 }
 
 void json_parse_array( json_object *jobj, char *key) {
-	void json_parse(json_object * jobj); /*Forward Declaration*/
+	void json_parse(json_object * jobj); //Forward Declaration
 	enum json_type type;
 
-	json_object *jarray = jobj; /*Simply get the array*/
+	json_object *jarray = jobj; //Simply get the array
 	if(key) {
-		jarray = json_object_object_get(jobj, key); /*Getting the array if it is a key value pair*/
+		jarray = json_object_object_get(jobj, key); //Getting the array if it is a key value pair
 	}
 
-	int arraylen = json_object_array_length(jarray); /*Getting the length of the array*/
+	int arraylen = json_object_array_length(jarray); //Getting the length of the array
 	printf("Array Length: %d\n", arraylen);
 	int i;
 	json_object * jvalue;
 
 	for (i=0; i< arraylen; i++){
-		jvalue = json_object_array_get_idx(jarray, i); /*Getting the array element at position i*/
+		jvalue = json_object_array_get_idx(jarray, i); //Getting the array element at position i//
 		type = json_object_get_type(jvalue);
 		if (type == json_type_array) {
 			json_parse_array(jvalue, NULL);
@@ -51,10 +52,10 @@ void json_parse_array( json_object *jobj, char *key) {
 	}
 }
 
-/*Parsing the json object*/
+//Parsing the json object
 void json_parse(json_object * jobj) {
 	enum json_type type;
-	json_object_object_foreach(jobj, key, val) { /*Passing through every array element*/
+	json_object_object_foreach(jobj, key, val) { //Passing through every array element//
 		printf("key: %s\n",key);
 		printf("type: %d ",type);
 		type = json_object_get_type(val);
@@ -73,11 +74,11 @@ void json_parse(json_object * jobj) {
 				break;
 		}
 	}
-} 
+} */
 
 string getStrFromKey(json_object * jobj, string skey) {
 	enum json_type type;
-	json_object_object_foreach(jobj, key, val) { /*Passing through every array element*/
+	json_object_object_foreach(jobj, key, val) { //Passing through every array element
 		if (string(key) == skey) {
 			type = json_object_get_type(val);
 			switch (type) {
