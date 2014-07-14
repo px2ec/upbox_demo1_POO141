a6594a8a97fd12d27610a93e3ea5da876b8e1320
Pedro Espinoza <pedropablo.espinoza@gmail.com>
2014-07-13 23:13:38 -0400

    Json
    Se separa la parte de procesamiento (parseo) y manipulación de objetos json.

---
+++ daemon/include/json_upbox.h
@@ -0,0 +1,11 @@
+
+#include <json/json.h>
+#include <stdio.h>
+
+#include <string>
+
+using namespace std;
+
+string getStrFromKey(json_object * jobj, string skey);
+int getIntFromKey(json_object * jobj, string skey);
+int getBooleanFromKey(json_object * jobj, string skey);
